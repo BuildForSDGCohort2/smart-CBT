2b8118d8e5f64a76e183d82b860dec6ee761c34a
htt-paul <paulobike98@gmail.com>
2020-09-09 04:04:22 +0100

    ft: define all exams routs

--- controllers/tables.js
+++ controllers/tables.js
@@ -7,7 +7,7 @@ module.exports = () => {
             id INT NOT NULL AUTO_INCREMENT,
             date DATETIME DEFAULT CURRENT_TIMESTAMP(),
             tutor VARCHAR(255),
-            courseTitle VARCHAR(255) NOT NULL,
+            courseTitle VARCHAR(255) NOT NULL UNIQUE,
             creditLoad INT,
             courseCode VARCHAR(255),
             examDuration INT,
@@ -18,5 +18,6 @@ module.exports = () => {
             if(err) console.log(err);
             else console.log('Exams table created')
         });
+        connection.release();
     });
 }
