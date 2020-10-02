e84deb9a6c6e1bc1406478cf6aa1c1f3e275b2de
htt-paul <paulobike98@gmail.com>
2020-10-02 02:08:08 +0100

    feat: connect to ai server
    this api can now mark theory questions

--- controllers/result.js
+++ controllers/result.js
@@ -1,7 +1,7 @@
 const pool    = require('./db-pool');
 const request = require('axios').create({
-    baseURL: 'https://smart-cbt-ai.herokuapp.com',
-    timeout: 1000,
+    baseURL: process.env.AI_API,
+    timeout: 5000,
     headers: {'Content-Type': 'application/json'}
   });
 
@@ -64,9 +64,10 @@ module.exports.createResult = (req, res) => {
     if(Object.keys(result).length === 0) {
         return res.status(400).json({message: 'Request body required.'})
     }
-    let regNo = req.params.id;
+    let regNo = req.body.regNo;
     let examId   = Number(req.params.examId);
     if(!examId) reject({status: 400, json:{message: 'Malformed EXAMID. EXAMID should be integer.'}});
+    if(!regNo) reject({status: 400, json:{message: 'Reg number is required in request body.'}});
     pool.getConnection((err, connection) => {
         if( err ) {
             console.log(err);
@@ -105,46 +106,55 @@ module.exports.createResult = (req, res) => {
                         console.log(err);
                         return res.status(500).json({message: 'Something went wrong.'});
                     }
+                    if(questions.length < result.responses.length) {
+                        return res.status(400).json({message: 'Questions don\'t match with those in database.'});
+                    }
                     if( examType === 'theory' ) {
                         let reqData = [];                    
                         result.responses.forEach(response => {
+                            response.solution = response.response;
+                            delete response.response;
                             let responseId    = response.id;
                             let matchedQuestionIndex = questions.findIndex(e => e.id === responseId);
                             let thisQuestion = { ...response, ...questions[matchedQuestionIndex] };
+                            thisQuestion.answers = JSON.parse(thisQuestion.answers);
                             reqData.push(thisQuestion);
                         });
-                        console.log(reqData);
-                        res.status(202).json({message: 'Request has been put in queue and will be processed soon.'});
-                        //send request to ai
-                        //recieve response
-                        //store result
-                        //send confirmation response
+                        if(reqData.length < 1) {
+                            return res.status(400).json({message: 'Questions don\'t match with those in database.'});
+                        }
+                        let reqBody = {questions: reqData}
+                        console.log(reqBody)
+                        request.post('/score', reqBody)
+                        .then(response => {
+                            console.log(response.data);
+                            let score = Math.ceil(((response.data.score / 2) / result.responses.length) * 100);
+                            let grade = getGrade(score);
+                            let dbResult = {regNo, score, grade, examId, summary: response.data.questions}
+                            saveResult(dbResult, connection, res);
+                        })
+                        .catch(err => {
+                            console.log(err.message);
+                            res.status(500).json({message: 'Something went wrong.'})
+                        });
                         
                     } else if( examType === 'objective' ) {
                         let correct = 0;
+                        let summary = [];
                         result.responses.forEach(response => {
                             let responseId    = response.id;
                             let matchedQuestionIndex = questions.findIndex(e => e.id === responseId);
-                            if( response.response === questions[matchedQuestionIndex]) {
+                            if( response.response === questions[matchedQuestionIndex].answers[0]) {
                                 correct++;
+                                summary.push({id: responseId, isCorrect: true})
+                            } else {
+                                summary.push({id: responseId, isCorrect: false})
                             }
                         });
-                        let score = (correct / result.responses.length) * 100;
-                        let grade = score < 40 ? 'F' : 
-                        (score < 45 ? 'E' : 
-                        (score < 50 ? 'D' : 
-                        (score < 60 ? 'C' : 
-                        (score < 70 ? 'B' : 'A'))))
-                        let dbResult = {
-                            regNo, 
-                            score,
-                            grade,
-                            examId
-                        }
-                        saveResult(dbResult, connection, res)
-                        //mark here
-                        //store result
-                        //send confirmation response
+                        let score = Math.ceil((correct / result.responses.length) * 100);
+                        let grade = getGrade(score);
+                        let dbResult = {regNo, score, grade, examId}
+                        saveResult(dbResult, connection, res);
                     }
                 });
             });
@@ -154,13 +164,35 @@ module.exports.createResult = (req, res) => {
 
 function saveResult(result, connection, res) {
     connection.query(`
-        INSERT INTO results
-        SET ?
-    `, result, (err, response, fields) => {
+        SELECT name FROM students
+        WHERE regNo = ?
+    `, result.regNo, (err, student, fields) => {
         if( err ) {
             console.log(err);
             return res.status(500).json({message: 'Something went wrong.'});
         }
-        res.json({message: 'Response submitted.'})
+        let studentName = student[0].name;
+        result.name = studentName;
+        result.summary = JSON.stringify(result.summary);
+        connection.query(`
+            INSERT INTO results
+            SET ?
+        `, result, (err, response, fields) => {
+            if( err ) {
+                console.log(err);
+                return res.status(500).json({message: 'Something went wrong.'});
+            }
+            res.json({message: 'Response submitted.', score: result.score})
+        })
     })
+    
+}
+
+function getGrade(score) {
+    let grade = score < 40 ? 'F' : 
+    (score < 45 ? 'E' : 
+    (score < 50 ? 'D' : 
+    (score < 60 ? 'C' : 
+    (score < 70 ? 'B' : 'A'))))
+    return grade;
 }
